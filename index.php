10c8d83da47be90573b49fbc54809d682f1139b3
Weslley Oliveira <weslleyoliveirawanted@gmail.com>
2020-07-26 17:44:55 -0400

    corrigido bug de não encontrar elemento na primeira posição

--- index.php
+++ index.php
@@ -9,17 +9,14 @@
                 <input type="text" id="phrase" name="phrase" placeholder="Buscar boletim" >
                 <input type="submit" value="Buscar boletim">
             </form>
-            <form action="" method="POST">
-                <input type="text" id="phrase" name="phrase" value="">
-                <input type="submit" value="Todos Boletins">
-            </form>
+
             <div id="lista" style="background-color: blue;">Lista de Boletins
                 <?php
-                    $boletins[0]['titulo'] = "Janeiro teve fortes chuvas";
+                    $boletins[0]['titulo'] = "Janeiro de 2020 teve fortes chuvas";
                     $boletins[0]['subtitulo'] = "Este é um subtitulo";
                     $boletins[1]['titulo'] = "Abril foi o mês com maior ocorrência de chuvas";
                     $boletins[1]['subtitulo'] = "Este é um subtitulo";
-                    $boletins[2]['titulo'] = "Agosto teve fortes chuvas";
+                    $boletins[2]['titulo'] = "Agosto de 2019 teve fortes chuvas";
                     $boletins[2]['subtitulo'] = "Este é um subtitulo";
                     $boletins[3]['titulo'] = "Maio teve fortes chuvas";
                     $boletins[3]['subtitulo'] = "Este é um subtitulo";
@@ -27,14 +24,21 @@
                     $boletins[4]['subtitulo'] = "Este é um subtitulo";
                     
                     if($_POST['phrase']!=null && $_POST['phrase']!=""){
+                        $count=0;
                         for($i=0;$i<=4;$i++){
                             if(strIpos($boletins[$i]['titulo'],$_POST['phrase'])>-1){
                                 echo $boletins[$i]['titulo'];echo "<br>";
                                 echo $boletins[$i]['subtitulo'];echo "<br>";
-
+                                $count++;
+                                echo $count;
+                                echo "<br>";
                             }
                         }
-                    }else{
+                        if($count==0){
+                            echo "Não foram encontrados bolentins.<br>";
+                        }
+                    }
+                    else{
                         for($i=0;$i<=4;$i++){
                             echo $boletins[$i]['titulo'];echo "<br>";
                             echo $boletins[$i]['subtitulo'];echo "<br>";
